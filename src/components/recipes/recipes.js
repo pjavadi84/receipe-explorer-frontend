adf85ea25e2883773238e08dba4ecb49e6937ccb
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2023-10-24 22:31:49 -0700

    wrapped the rendering logic and view inside the InfiniteScroll

--- src/components/recipes/recipes.js
+++ src/components/recipes/recipes.js
@@ -17,13 +17,29 @@ function RecipeList() {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div>
-      {data.recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.ingredients}</p>
+    <InfiniteScroll
+    dataLength={data.recipes.length}
+    next={() => {
+      fetchMore({
+        variables: {
+          offset: data.recipes.length
+        }
+      });
+    }}
+    hasMore={true} // determine this based on your data
+    loader={<h4>Loading...</h4>}
+    >
+        
+    {data.recipes.map(recipe => (
+       <div>
+        {data.recipes.map(recipe => (
+            <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.ingredients}</p>
+            </div>
+        ))}
         </div>
-      ))}
-    </div>
+    ))}
+  </InfiniteScroll>
   );
 }
